cb4243f5743408c4528923875843d2aba4177678
AllesonCoelho <alleson_henrique@hotmail.com>
2020-12-19 18:50:03 -0300

    Criando a Route

--- javascript_note/src/App.js
+++ javascript_note/src/App.js
@@ -2,15 +2,14 @@ import React, { Fragment } from 'react';
 import "./App.scss";
 import { Notification, Section } from 'rbx';
 import HomeScreen from './screens/home'
+import Routes from './routes'
 
-function App(){
-  return(
-    <Fragment>
-    <HomeScreen/>
-  </Fragment>
+const App = () => (
+  <div>
+    <Routes />
+  </div>
+)
 
-  )
-}
 
 
 export default App;
---
+++ javascript_note/src/routes.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import HomeScreen from './screens/home'
+
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path='/' component={HomeScreen} />
+        </Switch>
+    </BrowserRouter>
+)
+
+export default Routes
