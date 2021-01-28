06bdd4d9460087a9fd1f915829f499cdee624c44
AllesonCoelho <alleson_henrique@hotmail.com>
2021-01-28 20:27:23 -0300

    List Notes

--- javascript_note/src/components/notes/index.js
+++ javascript_note/src/components/notes/index.js
@@ -2,13 +2,36 @@ import React, { Fragment, useEffect, useState } from 'react';
 import { Column, Button } from "rbx";
 import "../../styles/notes.scss";
 import { push as Menu } from 'react-burger-menu'
+import List from '../notes/list/index'
+import NotesService from '../../services/notes'
 
 
 function Notes(props) {
+    const [notes, setNotes] = useState([]);
+    const [current_note, setCurrentNote] = useState({ title: "", body: "", id: "" });
+
+    useEffect(() => {
+        fetchNotes();
+    }, []);
+
+    async function fetchNotes() {
+        const response = await NotesService.index();
+        if (response.data.length >= 1) {
+            setNotes(response.data.reverse())
+            setCurrentNote(response.data[0])
+        }
+    }
+
+    const selectNote = (id) => {
+        const note = notes.find((note) => {
+            return note._id == id;
+        })
+        setCurrentNote(note);
+    }
 
     return (
         <Fragment>
-            <Column.Group className="notes" id="notes">
+            <div className="notes" id="notes">
                 <Menu
                     pageWrapId={"notes-editor"}
                     isOpen={props.isOpen}
@@ -16,21 +39,19 @@ function Notes(props) {
                     disableAutoFocus
                     outerContainerId={"notes"}
                     customBurgerIcon={false}
-                    customCrossIcon={false}>
-
-                    <Column.Group>
-                        <Column size={10} offset={1}>
-                            Search...
-                        </Column>
-                    </Column.Group>
-                    <p>List...</p>
+                    customCrossIcon={false}
+                >
+                    <List
+                        notes={notes}
+                        selectNote={selectNote}
+                        current_note={current_note} />
                 </Menu>
 
 
                 <Column size={12} className="notes-editor" id="notes-editor">
                     Editor...
-                </Column>
-            </Column.Group>
+          </Column>
+            </div>
         </Fragment>
     )
 }
