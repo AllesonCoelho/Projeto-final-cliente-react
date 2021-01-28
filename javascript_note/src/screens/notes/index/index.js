3a1249570e9b0767042b1cae2f2736d8d039a81e
AllesonCoelho <alleson_henrique@hotmail.com>
2021-01-28 19:12:53 -0300

    SideBar menu

--- javascript_note/src/screens/notes/index/index.js
+++ javascript_note/src/screens/notes/index/index.js
@@ -1,12 +1,19 @@
-import React, { Fragment } from 'react';
-import HeaderLogged from '../../../components/header_logged/index'
-import Notes from '../../components/notes/index'
+import React, { Fragment, useState } from 'react';
+import HeaderLogged from "../../../components/header_logged/index";
+import { Column } from "rbx";
+import "../../../styles/notes.scss";
 
-const NotesScreen = () => (
-    <Fragment>
-        <HeaderLogged/>
-        <Notes/>
-    </Fragment>
-);
+import Notes from "../../../components/notes/index";
+
+function NotesScreen() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+        <Fragment>
+            <HeaderLogged setIsOpen={setIsOpen} />
+            <Notes isOpen={isOpen} setIsOpen={setIsOpen} />
+        </Fragment>
+    )
+}
 
 export default NotesScreen;
